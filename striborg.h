c417deab9b87d35ff813d09720f9d9f44be2b823
Explosive-Cat <d1314995@yandex.ru>
2024-12-13 18:22:56 +0300

    striborg.h: created with exists functions

---
+++ striborg.h
@@ -0,0 +1,7 @@
+#include <stdint.h>
+
+int print_uint64(uint64_t b); //удалить по окончании разработки
+int X_512(uint8_t K[], uint8_t m[]);
+unsigned char pi_reflection(unsigned char input_V_8);
+unsigned char byte_swap(unsigned char input_S_64);
+uint64_t linear_trans(uint64_t b);
